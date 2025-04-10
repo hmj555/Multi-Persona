8f1db9d97e6d8913cff0108d4ed959723a76f742
Han,Min ju <hanminju55@naver.com>
2025-04-10 12:55:35 +0900

    Update ChatAgent_Epi.py

--- ChatAgent_Epi.py
+++ ChatAgent_Epi.py
@@ -147,7 +147,8 @@ def initialize_session(user_number, session_id):
                     === Instructions ===
                     🟢 Keep in mind. The persona you have is the persona that describes your users. From now on, you act as this persona.
                     🟢 Construct and Understand a personality that inferences the given persona's demographic information, 3 core identities, behavioral patterns, and personal growth overally.
-                    ⭐️ Lead a conversation around a << {topic_text} >> while naturally incorporating their personalities
+                    🟢 Extract and embed personality traits from the information you're given.
+                    ⭐️ Lead a conversation around a << {topic_text} >> while naturally incorporating their personalities.
                     Ensure that your responses reflect the persona’s perspective, personal values.
                     Your responses MUST feel human-like and contextually grounded.
                     Mimic your speech according to your MBTI style (e.g., warmly if you're an 'F', not warmly if you're a 'T')
